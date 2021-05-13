6ac1397a0cd75052d43453ce0201b20ec50c8f99
rafaelib <rafaeliunes97@gmail.com>
2021-05-13 04:12:45 -0300

    logica de assentos implementada parcialmente

--- src/Components/Seats.js
+++ src/Components/Seats.js
@@ -1,9 +1,51 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Seats = () => {
+  function selecionar() {
+    console.log("selecionado");
+  }
+  function avisar() {
+    alert("lugar indisponivel");
+  }
+
+  const [seats, setSeats] = useState([]);
+  const id = useParams().idSessao;
+  console.log(id);
+  useEffect(() => {
+    const request = axios.get(
+      `
+https://mock-api.bootcamp.respondeai.com.br/api/v2/cineflex/showtimes/${id}/seats
+`
+    );
+
+    request.then((response) => {
+      console.log(response.data.seats);
+      setSeats(response.data.seats);
+    });
+  }, []);
+  {
+    seats.map((i) => {
+      console.log(i);
+    });
+  }
   return (
     <main>
       <h4 className="step">Selecione o(s) assento(s)</h4>
+      <ul className="seats-list">
+        {seats.map((i) => {
+          const { id, name, isAvailable } = i;
+          console.log(isAvailable);
+          return (
+            <li
+              className={isAvailable ? "" : "unavailable"}
+              onClick={isAvailable ? selecionar : avisar}
+            ></li>
+          );
+        })}
+      </ul>
     </main>
   );
 };
--- src/Components/Sessions.js
+++ src/Components/Sessions.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Sessions = () => {
   const { idFilme } = useParams();
@@ -31,15 +32,19 @@ const Sessions = () => {
         <h4 className="step">Selecione o horário</h4>
         <ul className="days-list">
           {days.map((i) => {
+            const { id, date, weekday, showtimes } = i;
             return (
-              <li key={i.id}>
-                {console.log(i)}
+              <li key={id}>
                 <span className="date">
-                  {i.weekday} - {i.date}
+                  {weekday} - {date}
                 </span>
                 <div className="container-btns">
-                  <button>{i.showtimes[0].name}</button>
-                  <button>{i.showtimes[1].name}</button>
+                  <Link to={`/assentos/${showtimes[0].id}`}>
+                    <button>{showtimes[0].name}</button>
+                  </Link>
+                  <Link to={`/assentos/${showtimes[1].id}`}>
+                    <button>{showtimes[1].name}</button>
+                  </Link>
                 </div>
               </li>
             );
